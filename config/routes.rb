ea5a561734a30bf347c0f7357bf7b55e83d30c73
takashi miyasato <on.the.line.mt215@gmail.com>
2022-12-14 02:03:10 +0900

    deviseインストール

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root "main#index"
+  devise_for :users, controllers: {
+  registrations: 'users/registrations',
+  confirmations: 'users/confirmations',
+  passwords: 'users/passwords',
+  sessions: 'users/sessions',
+  unlocks: 'users/unlocks',
+  omniauth_callbacks: "users/omniauth_callbacks"
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
