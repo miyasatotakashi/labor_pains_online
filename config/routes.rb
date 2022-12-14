b54e25800f08089c3d6904cc51967be20e6569e0
takashi miyasato <on.the.line.mt215@gmail.com>
2022-12-14 13:22:42 +0900

    devise実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  root "main#index"
-  devise_for :users, controllers: {
+  get 'mains/index'
+  devise_scope :user do
+    root "mains#index"
+  end
+  devise_for :users,controllers: {
   registrations: 'users/registrations',
-  confirmations: 'users/confirmations',
-  passwords: 'users/passwords',
+  # confirmations: 'users/confirmations',
+  # passwords: 'users/passwords',
   sessions: 'users/sessions',
-  unlocks: 'users/unlocks',
-  omniauth_callbacks: "users/omniauth_callbacks"
-}
+  # unlocks: 'users/unlocks',
+  # omniauth_callbacks: "users/omniauth_callbacks"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
